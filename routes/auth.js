d83ec12a50074b99653b61a56367fff8c3d316c3
Chitra kadyan <chitrakadyan2005@gmail.com>
2025-06-11 00:11:31 +0530

    deletedfiles

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authcontrollers');
-
-router.post('/login', authController.login);
-
-module.exports = router; // Fixed typo (exports instead of export)
--- routes/hostels.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const hostelController = require('../controllers/hostelcontoller');
-
-router.get('/status/:id',hostelController.getStatus);
-router.post('/apply',hostelController.applyHostel);
-
-module.exports = router;
